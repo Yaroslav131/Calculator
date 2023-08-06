659842a858215e62b364c380681a46d8fb706a16
Yaroslav131 <berserk12186@gmail.com>
2023-08-06 17:48:21 +0300

    Add error boundery

---
+++ src/components/errorBoundary/index.tsx
@@ -0,0 +1,39 @@
+import { Component, ErrorInfo } from 'react';
+import { Text } from 'react-native';
+
+import { ErrorBoundaryProps, ErrorBoundaryState } from './types'
+import { ErrorContainer } from './styles';
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = {
+            hasError: false,
+            error: null,
+            errorInfo: null
+        };
+    }
+
+    componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+        this.setState({
+            hasError: true,
+            error: error,
+            errorInfo: errorInfo
+        });
+    }
+
+    render(error = this.state.error) {
+        if (this.state.hasError) {
+            return (
+                <ErrorContainer>
+                    <Text>Opss,Something went wrong.</Text>
+                    <Text> Try again later)))</Text>
+                    <Text>Error: {error && error.toString()}</Text>
+                </ErrorContainer>
+            );
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
---
+++ src/components/errorBoundary/styles.ts
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+
+import styled from 'styled-components/native';
+
+export const ErrorContainer = styled(View)`
+ flex:1;
+ justifycontent: 'center';
+ align-items: center;
+`;
